4eaab0109d7ca94ead4ebf466bcaf0bf56c93335
oono ta <taotaotao0310@gmail.com>
2018-03-22 16:31:56 +0900

    practice

---
+++ practice/welcometoatcoder.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+int main(void)
+{
+	int a;
+	int b,c;
+	std::string s;
+
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	std::cin >> s;
+
+	std::cout << a+b+c << " " << s << std::endl;
+
+	return 0;
+
+
+}
