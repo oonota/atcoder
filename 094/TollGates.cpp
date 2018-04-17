67544a929b7dc9b43995a29e82a623a3a8b47b19
oono ta <taotaotao0310@gmail.com>
2018-04-17 12:35:06 +0900

    Complete TollGates

--- 094/TollGates.cpp
+++ 094/TollGates.cpp
@@ -4,33 +4,55 @@
 
 int main(void){
 
-  int N,M,X;
+	int N,M,X;
 	int *a;
+	int *road;
 	std::string line;
 	std::string num;
+	int j = 0;
+	int middle;
 
 	std::cin >> N >> M >> X;
 	std::cin.ignore();
 
 
-  a = (int *)calloc(M,sizeof(int *));
+	a = (int *)calloc(M,sizeof(int *));
+	road = (int *)calloc(N+1,sizeof(int *));
 
 	std::getline(std::cin,line);
 	for(int i=0;i<M;i++){
-		a[i] = atoi(line[i]);
+		num = "";
+		for(;j<line.size()&&line[j] != ' ';j++){
+			num.push_back(line[j]);
+		}
+		j++;
+		a[i] = std::stoi(num);	
 	}
 
-	std::cout << N << std::endl;
-	std::cout << M << std::endl;
-	std::cout << X << std::endl;
+	for(int i=0;i<M;i++){
+    road[a[i]]++;	
+	}
 
-	for(int i;i<M;i++){
-		std::cout << a[i] << std::endl;
+	int low = 0;
+	int high = 0;
+
+	for(int i=X-1;i>0;i--){
+	low += road[i];
+	}
+	for(int i=X+1;i<N+1;i++){
+	high += road[i];
 	}
 
 
 
-	
+
+
+	if(low < high){
+		std::cout << low << std::endl;
+	}else{
+		std::cout << high << std::endl;
+	}	
+
 
 
 
