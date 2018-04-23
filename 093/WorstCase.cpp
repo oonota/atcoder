f99392505acf5b4824885b8c1e2ab808a2721348
oonota <taotaotao0310@gmail.com>
2018-04-23 21:01:21 +0900

    toriaezu kokomade

--- 093/WorstCase.cpp
+++ 093/WorstCase.cpp
@@ -9,6 +9,7 @@ int main(void){
 	long int *b;
 	long int count;
 	long int a_i,b_i;
+	int larger;
 
 	std::cin >> Q;
 	std::cin.ignore();
@@ -22,19 +23,23 @@ int main(void){
 		std::cin.ignore();
 	}
 
-	for(i=0;i<Q;++i){
-	std::cout << a[i] <<' '<< b[i]	<< std::endl;
-	
-	}
 
 	for(i=0;i<Q;++i){
 		count = 0;
+		a[i]>b[i]?larger=a[i]:larger=b[i];
+		std::cout << larger << std::endl;
 
-		for(a_i=1;a_i<a[i];++a_i)
-			for(b_i=1;b_i<b[i];++b_i)
-				if(a_i*b_i<a[i]*b[i]) ++count;
+
+		for(a_i=1;a_i<larger;++a_i){
+			if(a_i == a[i]) a_i++; 
+			for(b_i=1;b_i<larger;++b_i){
+				if(b_i == b[i]) b_i++;
+				if((a_i*b_i)<(a[i]*b[i])) {count++;b_i=larger;/*std::cout << a_i << b_i << std::endl;*/}
+
+			}
+		}
 		std::cout << count << std::endl;
-
+		return 0;
 	}
 	return 0;
 
