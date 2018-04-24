214c9303efd1526b73911201c6da88b272cbf3a2
oono ta <taotaotao0310@gmail.com>
2018-04-24 13:53:17 +0900

    complete Chocolate

---
+++ 092/Chocolate.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stdlib.h>
+
+#define DAY(i,j) (j)*(A[i])+1
+
+int main(void){
+
+	int N,D,X;
+	int *A;
+	int count=0;
+	int day=0;
+
+	std::cin >> N;	
+	std::cin.ignore();
+	std::cin >> D >> X;
+	std::cin.ignore();
+
+	A = (int *)calloc(N,sizeof(int *));
+	for(int i=0;i<N;i++){
+		std::cin >> A[i];
+		std::cin.ignore();
+	}
+
+	for(int i=0;i<N;i++){
+		day = 1;
+		for(int j=1;day<=D;j++){
+			count++;
+			day = DAY(i,j);
+		}
+	}
+	count += X;
+
+	std::cout << count << std::endl;
+
+	return 0;
+
+}
