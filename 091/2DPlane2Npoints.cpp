d9ff137c4d02abd75c3fbe53db4409c9f4ceb58c
oonota <taotaotao0310@gmail.com>
2018-04-28 16:44:34 +0900

    compile error

--- 091/2DPlane2Npoints.cpp
+++ 091/2DPlane2Npoints.cpp
@@ -1,34 +1,66 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<vector>
 
 #define print(x) std::cout << x << std::endl
+
+#define D 2
 
 int main(void){
 
 	int N;
-	int input[2];
-	std::vector<int> red;
-	std::vector<int> blue;
+	int count;
+	std::vector<int> input(N,0); 
+	std::vector<std::vector<int>> red;
+	std::vector<std::vector<int>> blue;
 
 
 	std::cin >> N;
+
 	for(int i=0;i<N;i++){
 		std::cin >> input[0] >> input[1];
+		//std::cin.ignore();
 		red.push_back(input);
+
 	}
 	for(int i=0;i<N;i++){
 		std::cin >> input[0] >> input[1];
-		//c.push_back(input[0]);
-		//d.push_back(input[1]);
-		//red.push_back({input[0],input[1]});
+		//std::cin.ignore();
+		blue.push_back(input);
+	}
+
+	std::sort(red.begin(),red.end());
+	std::sort(blue.begin(),blue.end());
+	printf("\n");
+	for(int i=0;i<N;i++){
+		for(int j=0;j<D;j++){
+			std::cout << (red[i][j]) << ' ';
+
+		}
+		std::cout << std::endl;
+	}
+	printf("\n");
+	for(int i=0;i<N;i++){
+		for(int j=0;j<D;j++){
+			std::cout << (blue[i][j]) << ' ';
+
+		}
+		std::cout << std::endl;
+	}
+	count = 0;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<blue.size();j++){
+			if(red[i][0]<blue[j][0] && red[i][1]<blue[j][1]){
+				blue.erase(blue.begin()+ j);
+				count++;
+				j = N;
+
+			}	
+		}
 	}
 
 
+	return 0;	
 
-
-
-
-	return 0;
 
 }
