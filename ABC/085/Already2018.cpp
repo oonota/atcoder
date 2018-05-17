261538611316947436d268379cf5ca08572ec6e5
oono ta <taotaotao0310@gmail.com>
2018-05-17 09:22:46 +0900

    Complete Already2018

---
+++ ABC/085/Already2018.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+int main(void){
+
+	std::string input;
+
+	std::cin >> input;
+	input[3] = '8';
+
+	print(input);
+	
+	return 0;
+
+}
