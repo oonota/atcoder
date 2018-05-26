470d2a0ffaa4e573d70d71e82c9a0db5a6f18bfc
oonota <taotaotao0310@gmail.com>
2018-05-26 11:03:30 +0900

    Complete RoundUptheMean

---
+++ ABC/082/RoundUptheMean.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+int main(void){
+
+	int X,Y;
+
+	std::cin >> X >> Y;
+
+	int ave;
+	ave = (int)((X+Y)/2.0+0.5);
+
+	print(ave);
+
+	return 0;
+
+}
