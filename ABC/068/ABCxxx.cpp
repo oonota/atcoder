5cdc60c5001f206957b53f88c5c00b9a669a49f9
oono ta <taotaotao0310@gmail.com>
2018-06-29 11:10:36 +0900

    ABCxxx

---
+++ ABC/068/ABCxxx.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+int main(void){
+
+	int N;
+	std::cin >> N;
+
+	std::cout << "ABC" << N << std::endl;
+
+
+	return 0;
+
+}
