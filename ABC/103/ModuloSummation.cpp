e1fe03e0525cb2e83eb0ea6c859a56bc25d5260e
oono ta <taotaotao0310@gmail.com>
2018-07-24 11:11:48 +0900

    ModuloSummation

--- ABC/103/ModuloSummation.cpp
+++ ABC/103/ModuloSummation.cpp
@@ -13,25 +13,11 @@ int main(void){
 	for(int i=0;i<N;++i)
 		std::cin >> a[i];
 
-	std::sort(a.begin(),a.end());
-	int big = a[N-1]-1;
-	int flag = 0;
-	int f = 0;
-	int m = 0;
-	for(int i=0;flag<3;i++){
-		flag = 0;
-		f = 0;
-		m += big;
-		//print(m);
-		for(int j=0;j<N;j++){
-			if(m%a[j]==a[j]-1){
-				flag++;
-			}
-			f += m%a[j];
-		}
+	int mod = 0;
+	for(int i=0;i<N;++i)
+		mod += a[i]-1;
 
-	}
-	print(f);
+	print(mod);
 
 
 	return 0;
