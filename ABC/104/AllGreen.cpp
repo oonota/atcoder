38c116dda82ba79ae44d390202e93dddd6bb9b79
oonota <taotaotao0310@gmail.com>
2018-08-06 06:21:03 +0900

    a

--- ABC/104/AllGreen.cpp
+++ ABC/104/AllGreen.cpp
@@ -12,20 +12,25 @@ int main(void){
 	for(int i=0;i<D;++i)
 		std::cin >> p[i] >> c[i];
 
-	std::vector<int> tokuten(D,0);
-	for(int i=0;i<=D;++i){
-		int ii = i+1; 
-		tokuten[i] = 100*ii*p[i] + c[i];
-	}
-	for(int i=0;i<D;++i){
-		if(tokuten[i] >= G){
-			print(p[i]);
+	int tokuten = 0;
+	int item_num = 0;
+	int min = 0;
+	for(int i = 0; i < D; ++i){
+		for(int j=0;j<p[i];++j){
+			tokuten += 100*(i+1);
+			item_num++;
+		}
+		tokuten += c[i];
+		int single = 100*(i+1)*p[i] + c[i];
+		if(tokuten >= G && single >= G){
+			min = item_num<=p[i]?item_num:p[i];
+			break;
+		}else if(tokuten >= G){
+			min = item_num;
 			break;
 		}
-
 	}
-
-
+	print(min);
 
 	return 0;
 }
