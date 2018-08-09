0c34320e35f1c3e02baa5c1fb7af5a4af9a7eb8d
oonota <taotaotao0310@gmail.com>
2018-08-10 07:55:55 +0900

    ABCARC

---
+++ ABC/053/ABCARC.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+int main(void){
+
+	int x;
+	std::cin >> x;
+
+	if(x<1200)
+		print("ABC");
+	else
+		print("ARC");
+
+	return 0;
+}
