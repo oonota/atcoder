8f18461b107ea2f4a0285065e43a5a7f4073bbbc
oonota <taotaotao0310@gmail.com>
2018-08-25 09:10:58 +0900

    fix TLE

--- ABC/051/SumofThreeInteger.cpp
+++ ABC/051/SumofThreeInteger.cpp
@@ -14,9 +14,8 @@ int main(void){
 	int count = 0;
 	for(int X=0;X<=K;++X){
 		for(int Y=0;Y<=K;++Y){
-			for(int Z=0;Z<=K;++Z){
-				if(X+Y+Z == S) count++;
-			}
+			int Z = S - X - Y;
+				if(0 <= Z && Z <= K) count++;
 		}
 	}
 	print(count);
