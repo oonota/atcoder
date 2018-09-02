b630d31921877ec51f5b660ae2fd6463d00e29a8
oonota <taotaotao0310@gmail.com>
2018-09-02 11:43:11 +0900

    108

---
+++ ABC/108/Pair.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int K; cin >> K;
+
+	print((K/2)*((K+1)/2));
+
+
+	return 0;
+
+}
