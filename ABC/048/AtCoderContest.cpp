e2229ba8255a98f1a0a99bca461b5ebc2faecccf
oono ta <taotaotao0310@gmail.com>
2018-09-05 12:43:09 +0900

    048

---
+++ ABC/048/AtCoderContest.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	string a;
+	string b;
+	string c;
+	cin >> a >> b >> c;
+
+	cout << a[0] << b[0] << c[0] << endl;
+
+
+	return 0;
+
+}
