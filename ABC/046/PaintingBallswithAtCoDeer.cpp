517faccfed731aa2bc8fb419d5dfb3b3dcc834c7
oonota <taotaotao0310@gmail.com>
2018-09-22 17:39:51 +0900

    PaintingBalls

---
+++ ABC/046/PaintingBallswithAtCoDeer.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int N,K;
+	cin >> N >> K;
+
+
+	int ans = K;
+	REP(i,N-1) ans *= K-1;
+
+	print(ans);
+
+
+	return 0;
+
+}
