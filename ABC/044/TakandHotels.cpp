6e78ec381ce524c8c7995f6aa89c65ccd172fa22
oonota <taotaotao0310@gmail.com>
2018-09-29 12:14:38 +0900

    asd

---
+++ ABC/044/TakandHotels.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+#define print(x) std::cout << x << std::endl
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+
+//container util
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+
+
+int main(void){
+
+	int N,K,X,Y;
+	cin >> N >> K >> X >> Y;
+
+	int ans = K*X+(N-K)*Y;
+	print(ans);
+
+
+	return 0;
+
+}
